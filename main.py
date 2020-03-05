93a8a4f621c5fc6dc72a375d06ecedaf0a3d9965
mateus <mateusfereira17@yahoo.com.br>
2020-03-05 18:52:23 -0300

    segundo sprint lab

--- main.py
+++ main.py
@@ -5,27 +5,37 @@ import requests
 query_type = ['search', 'user', 'organization', 'repository']
 search_type = ['search', 'user', 'organization', 'repository']
 length = 0
+after = ' '
 
-
-quersy = 'query example {search(query:"stars:>100",type:REPOSITORY, first:100){nodes {... on Repository {nameWithOwner ' \
-         'diskUsage}}}} '
-print(quersy)
-
-
-def run_query(query):
+query = 'query{search(query:"stars:>100", type:REPOSITORY, first:100' + after + '){' \
+                                                                                'pageInfo{hasNextPage endCursor}' \
+                                                                                'nodes{... ' \
+                                                                                'on Repository{' \
+                                                                                'nameWithOwner url createdAt}}}}'
+dado = ''
+print(query)
+have_next_page = True
+page = 1
+nodes = list()
+while have_next_page and page < 10:
     request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
-    if request.status_code == 200:
-        print(request.json())
-        return request.json()
-    else:
-        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+    print(request.status_code)
 
+    if request.status_code == 200:
+        result = request.json()
+
+        nodes += result['data']['search']['nodes']
+
+        have_next_page = result["data"]["search"]["pageInfo"]["hasNextPage"]
+        page += 1
+        cursor = result["data"]["search"]["pageInfo"]["endCursor"]
+        after = ", after:{}".format(cursor)
+    print(nodes)
 
 names = []
 to_csv = []
-dados = run_query(quersy)
-print(dados['data']['search']['nodes'])
-for d in dados['data']['search']['nodes']:
+
+for d in nodes:
     row = {}
     for keys in d.keys():
         if keys not in names:
@@ -33,7 +43,7 @@ for d in dados['data']['search']['nodes']:
         row.update({keys: d[keys]})
     to_csv.append(row)
 
-with open('amis.csv', mode='w', encoding='utf-8', newline='') as csvfile:
+with open('resultados.csv', mode='w', encoding='utf-8', newline='') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=names, delimiter=';')
     writer.writeheader()
     for i in to_csv:
